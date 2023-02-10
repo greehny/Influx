d17d0cbda8c4566fc77e0f8303c335ea3dc70c1f
greehnu <betsyrage@gmail.com>
2023-02-10 00:49:49 -0500

    fixed harvesting method

--- src/main/java/com/github/greehny/Influx/mining/CustomMineFunction.java
+++ src/main/java/com/github/greehny/Influx/mining/CustomMineFunction.java
@@ -2,19 +2,18 @@ package com.github.greehny.Influx.mining;
 
 import com.github.greehny.Influx.InfluxMain;
 import com.github.greehny.Influx.items.CustomItems;
-import com.jeff_media.customblockdata.CustomBlockData;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.persistence.PersistentDataContainer;
-import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
 public class CustomMineFunction implements Listener {
     private InfluxMain influxMain;
@@ -23,27 +22,37 @@ public class CustomMineFunction implements Listener {
     }
 
     CustomItems customItems = new CustomItems();
+
     @EventHandler
     public void onMine(PlayerInteractEvent event){
         Block block = event.getClickedBlock();
         Player player = event.getPlayer();
-        ItemStack itemHand = event.getItem();
+        ItemStack itemHand = player.getItemInHand();
 
         if (block == null) { return; }
-
         if (event.getAction() != Action.RIGHT_CLICK_BLOCK || event.getHand() != EquipmentSlot.HAND) { return; }
-
-        if (itemHand.getType() != Material.WOODEN_PICKAXE || itemHand.getType() != Material.STONE_PICKAXE || itemHand.getType() != Material.IRON_PICKAXE){
-            return;
-        }
-
-        if (!influxMain.getCustomConfig().contains("nodes.ore.location." + block.getX() + "-" + block.getY() + "-" + block.getZ())) { return; }
-        if (!influxMain.getCustomConfig().getString("nodes.ore.location." + block.getX() + "-" + block.getY() + "-" + block.getZ() + ".oretype").equals("lunarite-ore")) {return;}
-        influxMain.getCustomConfig().set("nodes.ore.location." + block.getX() + "-" + block.getY() + "-" + block.getZ(), null);
+        if (itemHand.getType() != Material.IRON_PICKAXE) { return; }
+        if (block.getType() != Material.GOLD_ORE) { return; }
+        if (!influxMain.getCustomConfig().contains("nodes.ore." + block.getX() + "-" + block.getY() + "-" + block.getZ())) { return; }
+        if (!influxMain.getCustomConfig().getString("nodes.ore." + block.getX() + "-" + block.getY() + "-" + block.getZ() + ".oretype").equals("lunarite-ore")) {return;}
         influxMain.saveCustomConfig();
+        player.getInventory().addItem(customItems.getLunarite());
+        block.setType(Material.BEDROCK);
 
-        block.getWorld().dropItemNaturally(block.getLocation(), customItems.getLunarite());
-        block.setType(Material.AIR);
+       BukkitTask regen = new BukkitRunnable() {
+            @Override
+            public void run() {
+                block.setType(Material.GOLD_ORE);
+            }
+        }.runTaskLater(influxMain, 20L * 10L);
     }
 
+    @EventHandler
+    public void onCustomBlockBreak(BlockBreakEvent event) {
+        Block block = event.getBlock();
+        if (influxMain.getCustomConfig().contains("nodes.ore." + block.getX() + "-" + block.getY() + "-" + block.getZ())) {
+            influxMain.getCustomConfig().set("nodes.ore." + block.getX() + "-" + block.getY() + "-" + block.getZ(), null);
+            influxMain.saveCustomConfig();
+        }
+    }
 }
